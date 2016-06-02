8a164faef8a961afa7e040f8299110b09d7ff337
Robert Coltheart <robert.coltheart@gmail.com>
2016-06-02 17:30:06 +0800

    Fix assemblyinfo

--- src/Conductor/Properties/AssemblyInfo.cs
+++ src/Conductor/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Conductor")]
-[assembly: AssemblyDescription("A simple framework for building modular applications.")]
+[assembly: AssemblyDescription("A framework for building modular applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Conductor")]
@@ -30,7 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.1.0")]
-[assembly: AssemblyFileVersion("3.1.1.0")]
-
-[assembly: AssemblyInformationalVersion("3.1.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
